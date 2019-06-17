312186b5723b26fc6475a98b4b80b47b14a7d6bd
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-06-17 13:48:39 -0700

    Finished most of it except for the nav bar. Working on this now

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// let nav1=document.querySelectorAll("nav a");
+
+// nav1[0].textContent = siteContent["nav"]["nav-item-1"];
+// nav1[1].textContent = siteContent["nav"]["nav-item-2"];
+// nav1[2].textContent = siteContent["nav"]["nav-item-3"];
+// nav1[3].textContent = siteContent["nav"]["nav-item-4"];
+// nav1[4].textContent = siteContent["nav"]["nav-item-5"];
+// nav1[5].textContent = siteContent["nav"]["nav-item-6"];
+
+const firstheader = document.querySelector("h1");
+ firstheader.textContent= siteContent["cta"]["h1"];
+
+ const button = document.querySelector("button");
+ button.textContent = siteContent["cta"]["button"];
+
+ let ctaimg = document.getElementById("cta-img");
+ ctaimg.src = siteContent["cta"]["img-src"];
+ 
+const h4 = document.querySelectorAll(".main-content h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"]
+h4[1].textContent = siteContent["main-content"]["about-h4"]
+h4[2].textContent = siteContent["main-content"]["services-h4"]
+h4[3].textContent = siteContent["main-content"]["vision-h4"]
+
+const p = document.querySelectorAll(".main-content p");
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["vision-content"];
+
+let midimg = document.getElementById("middle-img");
+midimg.src = siteContent["main-content"]["middle-img-src"];
+
+const h4contact = document.querySelector(".contact h4");
+h4contact.textContent = siteContent["contact"]["contact-h4"];
+
+const pcontact = document.querySelectorAll(".contact p");
+pcontact[0].textContent = siteContent["contact"]["address"];
+pcontact[1].textContent = siteContent["contact"]["phone"];
+pcontact[2].textContent = siteContent["contact"]["email"];
+
+const footer = document.querySelector("footer p");
+footer.textContent = siteContent["footer"]["copyright"];
+
+
+
